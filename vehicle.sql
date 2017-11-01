14463ed0505a547ffa5e83bf0b9f413869931823
Devansh <devansh.sharma@gmail.com>
2017-11-02 00:27:19 +0530

    Updated DB

--- vehicle.sql
+++ vehicle.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.19, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
 --
--- Host: localhost    Database: PROJECT
+-- Host: localhost    Database: VEHICLE_RENTAL
 -- ------------------------------------------------------
--- Server version	5.7.19-0ubuntu0.16.04.1
+-- Server version	5.7.20-0ubuntu0.16.04.1-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,34 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `BikeDetails`
---
-
-DROP TABLE IF EXISTS `BikeDetails`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `BikeDetails` (
-  `Plate_No` varchar(10) NOT NULL,
-  `Company` varchar(20) DEFAULT NULL,
-  `Model` varchar(20) DEFAULT NULL,
-  `Color` varchar(10) DEFAULT NULL,
-  `Type` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`Plate_No`),
-  CONSTRAINT `BikeDetails_ibfk_1` FOREIGN KEY (`Plate_No`) REFERENCES `Vehicle` (`Plate_No`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `BikeDetails`
---
-
-LOCK TABLES `BikeDetails` WRITE;
-/*!40000 ALTER TABLE `BikeDetails` DISABLE KEYS */;
-INSERT INTO `BikeDetails` VALUES ('KA05DE6587','Kawasaki','Ninja H2R','Green','Sports');
-/*!40000 ALTER TABLE `BikeDetails` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `Booking`
 --
 
@@ -51,15 +23,16 @@ DROP TABLE IF EXISTS `Booking`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Booking` (
+  `Book_ID` varchar(10) NOT NULL,
   `User_ID` varchar(20) DEFAULT NULL,
   `Plate_No` varchar(10) DEFAULT NULL,
-  `Book_ID` varchar(10) NOT NULL,
-  `Pay_ID` varchar(10) DEFAULT NULL,
   `Book_Date` date DEFAULT NULL,
   `No_of_Days` int(11) DEFAULT NULL,
   PRIMARY KEY (`Book_ID`),
+  KEY `User_ID` (`User_ID`),
   KEY `Plate_No` (`Plate_No`),
-  CONSTRAINT `Booking_ibfk_1` FOREIGN KEY (`Plate_No`) REFERENCES `Vehicle` (`Plate_No`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `Booking_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `User` (`User_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Booking_ibfk_2` FOREIGN KEY (`Plate_No`) REFERENCES `VehicleDetails` (`Plate_No`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -73,35 +46,6 @@ LOCK TABLES `Booking` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `CarDetails`
---
-
-DROP TABLE IF EXISTS `CarDetails`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `CarDetails` (
-  `Plate_No` varchar(10) NOT NULL,
-  `Company` varchar(20) DEFAULT NULL,
-  `Model` varchar(20) DEFAULT NULL,
-  `Color` varchar(10) DEFAULT NULL,
-  `Type` varchar(10) DEFAULT NULL,
-  `Seats` int(11) DEFAULT NULL,
-  PRIMARY KEY (`Plate_No`),
-  CONSTRAINT `CarDetails_ibfk_1` FOREIGN KEY (`Plate_No`) REFERENCES `Vehicle` (`Plate_No`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `CarDetails`
---
-
-LOCK TABLES `CarDetails` WRITE;
-/*!40000 ALTER TABLE `CarDetails` DISABLE KEYS */;
-INSERT INTO `CarDetails` VALUES ('KA08SK9283','Hennessey','Venom GT','Black','Sports',4);
-/*!40000 ALTER TABLE `CarDetails` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `Employee`
 --
 
@@ -109,15 +53,16 @@ DROP TABLE IF EXISTS `Employee`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Employee` (
-  `Emp_ID` varchar(10) NOT NULL,
-  `Emp_Name` varchar(20) DEFAULT NULL,
-  `Email_ID` varchar(30) DEFAULT NULL,
+  `User_ID` varchar(20) NOT NULL,
+  `Name` varchar(20) DEFAULT NULL,
+  `Email` varchar(20) DEFAULT NULL,
   `Phone_No` bigint(20) DEFAULT NULL,
+  `Address` varchar(20) DEFAULT NULL,
   `Password` varchar(20) DEFAULT NULL,
   `G_ID` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`Emp_ID`),
+  PRIMARY KEY (`User_ID`),
   KEY `G_ID` (`G_ID`),
-  CONSTRAINT `Employee_ibfk_1` FOREIGN KEY (`G_ID`) REFERENCES `Garage` (`G_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `Employee_ibfk_1` FOREIGN KEY (`G_ID`) REFERENCES `Garage` (`G_ID`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -140,11 +85,8 @@ DROP TABLE IF EXISTS `Garage`;
 CREATE TABLE `Garage` (
   `G_ID` varchar(10) NOT NULL,
   `G_Name` varchar(20) DEFAULT NULL,
-  `Location` varchar(20) DEFAULT NULL,
-  `Book_ID` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`G_ID`),
-  KEY `Book_ID` (`Book_ID`),
-  CONSTRAINT `Garage_ibfk_1` FOREIGN KEY (`Book_ID`) REFERENCES `Booking` (`Book_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+  `LOCATION` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`G_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -167,12 +109,16 @@ DROP TABLE IF EXISTS `Payment`;
 CREATE TABLE `Payment` (
   `Pay_ID` varchar(10) NOT NULL,
   `User_ID` varchar(20) DEFAULT NULL,
+  `Book_ID` varchar(10) DEFAULT NULL,
   `Pay_Date` date DEFAULT NULL,
   `Amount` int(11) DEFAULT NULL,
-  `Type` varchar(10) DEFAULT NULL,
+  `Method` varchar(10) DEFAULT NULL,
+  `Success` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`Pay_ID`),
   KEY `User_ID` (`User_ID`),
-  CONSTRAINT `Payment_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `User` (`User_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `Book_ID` (`Book_ID`),
+  CONSTRAINT `Payment_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `User` (`User_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Payment_ibfk_2` FOREIGN KEY (`Book_ID`) REFERENCES `Booking` (`Book_ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -195,9 +141,9 @@ DROP TABLE IF EXISTS `User`;
 CREATE TABLE `User` (
   `User_ID` varchar(20) NOT NULL,
   `Name` varchar(20) DEFAULT NULL,
-  `Email_ID` varchar(30) DEFAULT NULL,
+  `Email` varchar(20) DEFAULT NULL,
   `Phone_No` bigint(20) DEFAULT NULL,
-  `Address` varchar(50) DEFAULT NULL,
+  `Address` varchar(20) DEFAULT NULL,
   `Password` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`User_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -209,32 +155,63 @@ CREATE TABLE `User` (
 
 LOCK TABLES `User` WRITE;
 /*!40000 ALTER TABLE `User` DISABLE KEYS */;
-INSERT INTO `User` VALUES ('ABC123','User1','user1@email.com',9875546254,'Random_Location_1','best_password_1'),('DEF456','User2','user2@email.com',8745526698,'Random_Location_2','best_password_2');
 /*!40000 ALTER TABLE `User` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Vehicle`
+-- Table structure for table `VehicleDetails`
 --
 
-DROP TABLE IF EXISTS `Vehicle`;
+DROP TABLE IF EXISTS `VehicleDetails`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Vehicle` (
+CREATE TABLE `VehicleDetails` (
   `Plate_No` varchar(10) NOT NULL,
-  `Type` varchar(4) DEFAULT NULL,
-  PRIMARY KEY (`Plate_No`)
+  `Model_Name` varchar(20) DEFAULT NULL,
+  `Color` varchar(10) DEFAULT NULL,
+  `G_ID` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`Plate_No`),
+  KEY `Model_Name` (`Model_Name`),
+  KEY `G_ID` (`G_ID`),
+  CONSTRAINT `VehicleDetails_ibfk_1` FOREIGN KEY (`Model_Name`) REFERENCES `Vehicles` (`Model_Name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `VehicleDetails_ibfk_2` FOREIGN KEY (`G_ID`) REFERENCES `Garage` (`G_ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Vehicle`
+-- Dumping data for table `VehicleDetails`
 --
 
-LOCK TABLES `Vehicle` WRITE;
-/*!40000 ALTER TABLE `Vehicle` DISABLE KEYS */;
-INSERT INTO `Vehicle` VALUES ('KA05DE6587','Bike'),('KA08SK9283','Car');
-/*!40000 ALTER TABLE `Vehicle` ENABLE KEYS */;
+LOCK TABLES `VehicleDetails` WRITE;
+/*!40000 ALTER TABLE `VehicleDetails` DISABLE KEYS */;
+/*!40000 ALTER TABLE `VehicleDetails` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Vehicles`
+--
+
+DROP TABLE IF EXISTS `Vehicles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Vehicles` (
+  `Model_Name` varchar(20) NOT NULL,
+  `Company` varchar(20) DEFAULT NULL,
+  `V_Type` varchar(4) DEFAULT NULL,
+  `Type` varchar(10) DEFAULT NULL,
+  `Seats` int(11) DEFAULT NULL,
+  `Quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Model_Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Vehicles`
+--
+
+LOCK TABLES `Vehicles` WRITE;
+/*!40000 ALTER TABLE `Vehicles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Vehicles` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -246,4 +223,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-23 21:12:13
+-- Dump completed on 2017-11-02  0:24:14
